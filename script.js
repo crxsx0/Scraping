fd5366d2efe712b657f13358e4a346779835a26c
crxsx0 <crxsx.0@gmail.com>
2023-04-19 23:39:49 -0400

    prueba funcion

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const puppeteer = require('puppeteer');
 
+function obtenerHref (lista){
+
+    const listaFinal = [];
+
+    for (i of lista){
+        listaFinal.push(i.href);
+    }
+
+    return listaFinal;
+}
+
+
+/*async function unidadCurso (url){
+    await page.goto(url);
+    await page.waitForSelector('#menu_recursos_oa');
+    page.click("#menu_recursos_oa > ul > li:nth-child(3) > a", puppeteer.MouseButton);
+    await page.waitForSelector("#recuadros_actividades");
+
+    const linksRecursosUnidad = await page.evaluate(() => {
+        const linksRecursos =  obtenerHref ("div.thumbnail a");
+
+
+    });
+}*/
+
+
 async function scrap (){
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
@@ -8,15 +34,9 @@ async function scrap (){
     await page.setViewport({width:1920, height: 1080});
 
     const linksInicio = await page.evaluate(() => {
-        const listaLinksInicio = document.querySelectorAll("div.row.parvularia.menu-cursos-general.fondo-cuadrados ul li a");
-
-        const linksCursos = [];
-
-        for (i of listaLinksInicio){
-            linksCursos.push(i.href);
-        }
-
-        return linksCursos;
+        const links = document.querySelectorAll("div.row.parvularia.menu-cursos-general.fondo-cuadrados ul li a");
+        const listaa = obtenerHref(links)
+        return listaa;
     });
 
     console.log(linksInicio);
